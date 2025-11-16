186422c909b314cffabea9d8a48fcdde99f88f33
abdulmalikaljunaid <aboodaljunaid@gmail.com>
2025-11-17 00:48:58 +0300

    fix: resolve TypeScript error in cache - check for undefined key

--- lib/cache.ts
+++ lib/cache.ts
@@ -15,7 +15,9 @@ class SimpleCache {
     // Remove oldest entries if cache is full
     if (this.cache.size >= this.maxSize) {
       const firstKey = this.cache.keys().next().value;
-      this.cache.delete(firstKey);
+      if (firstKey) {
+        this.cache.delete(firstKey);
+      }
     }
 
     this.cache.set(key, {
